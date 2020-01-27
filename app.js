1f374fcc9311a90f1b1b32adabf6e96c5d10c77f
Igor Risteski <pirej30@yahoo.com>
2020-01-27 02:09:32 +0100

    Use Passport.js for authentication and encryption register/login/ and sending a session cookie works logout works

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require('passport-local-mongoose');
 
 
 const app = express();
@@ -17,8 +18,23 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/// initialize and start using the session package
+/// using an initial configuration
+app.use(session({
+    secret: "tajnata vecera",
+    resave: false,
+    saveUninitialized: false
+}));
+
+//// tell the app to initialize and use the passport package,
+app.use(passport.initialize());
+/// tell the app to use passport.. to deal with session
+app.use(passport.session());
+
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+/// fix DeprecationWarning  https://github.com/Automattic/mongoose/issues/6890
+mongoose.set('useCreateIndex', true);
 
 // ///setup a new user database
 /// setup a proper mongoose schema(object created from the schema class)
@@ -27,10 +43,19 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-
+/// https://www.npmjs.com/package/passport-local-mongoose
+/// User.plugin(passportLocalMongoose);
+/// set our userSchema to use passportLocalMongoose as plugin
+userSchema.plugin(passportLocalMongoose);
 
 //// new mongoose Model for DB, with collection name "User", using the userSchema
 const User = new mongoose.model("User", userSchema);
+
+/// Simplified Passport/Passport-Local Configuration
+/// https://www.npmjs.com/package/passport-local-mongoose
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //// show the pages when url is typed
 app.get("/", function (req, res) {
@@ -42,64 +67,57 @@ app.get("/login", function (req, res) {
 app.get("/register", function (req, res) {
     res.render("register");
 });
+app.get("/secrets", function (req, res) {
+    /// if the user isAuthenticated, send/redirect him to secrets page
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+        /// else, redirect the user back to login page
+    } else {
+        res.redirect("login");
+    }
+});
+
+/// logout -> de-authenticate 
+/// http://www.passportjs.org/docs/logout/
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+});
 
 //// register the user, and save the input to userDB
 app.post("/register", function (req, res) {
-
-    //// Technique 2 (auto-gen a salt and hash): 
-    //// https://www.npmjs.com/package/bcrypt
-    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-        ///////// Store hash in your password DB. ////////////////
-        ///create new user, using the userSchema and the input from register page form 
-        const newUser = new User({
-            email: req.body.username,
-            //// hash the password
-            password: hash
-        });
-        ///// save the new registered user
-        newUser.save(function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                //// show the sicrets page only if/when user is registered
-                res.render("secrets");
-            }
-        });
-    });
-
-});
-///login the registered user
-app.post("/login", function (req, res) {
-    ///// we need to check two things, username, and pass, so we declare them here
-    const userName = req.body.username;
-    const password = req.body.password;
-
-    //// here we check if the entered credentials exist in the userDB
-    //// in collection "User", findOne registered email: thats same with the entered username
-    User.findOne({ email: userName }, function (err, foundUser) {
-        if (err) {
+    /// usage of the User. is possible because of the passportLocalMongoose package
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
+        if (err) { /// if error, log it, and redirect the user back to register page
             console.log(err);
-        } else {
-            //// if there is a matching user found..
-            if (foundUser) {
-                ////then check if that foundUser in DB.. has a (registered)password -> hash in DB, with a hash that matches the hash from your password DB(foundUser.password).
-                // bcrypt.compare(myPlaintextPassword, hash, function (err, res) { 
-                bcrypt.compare(password, foundUser.password, function (err, result) {
-                    // if resault is true
-                    if (result === true) {
-                        ///if hash matches, show the "secret" page
-                        console.log("you got the password!!!");
-                        res.render("secrets");
-                    } else {
-                        console.log("wrong password!!!");
-                    }
-                });
-            }
+            res.redirect("/register");
+        } else { /// if the user is successfully authenticated, redirect them to "secrets" page, and send a cookie to the browser
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
         }
     });
 });
 
+///login the registered user
+app.post("/login", function (req, res) {
+    /// create a new user from our mongoose model 
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    ///http://www.passportjs.org/docs/login/
+    req.login(user, function (err) {
+        if (err) {
+            console.log(err);
+        } else { /// if the user is successfully authenticated, redirect them to "secrets" page, and send a cookie to the browser
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
+        }
+    });
 
+});
 
 
 
